b9b8dade660082abbe1d4043ce7f191344ce196f
Nazmul-Huq <hmnazmulhuq@gmail.com>
2022-02-28 10:11:34 +0100

    first setup

---
+++ src/main/java/com/example/dmm/DmmApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dmm/DmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
